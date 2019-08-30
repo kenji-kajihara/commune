ae5183dec4a92e7a5842ca2cbd162cc724e45e36
kenji <kenji.kajihara712@gmail.com>
2019-08-30 09:30:43 +0000

    UPDATE : en to ja

---
+++ resources/lang/ja/messages.php
@@ -0,0 +1,15 @@
+<?php
+
+
+return [
+    'Login' => 'ログイン',
+    'E-Mail Address' => 'メールアドレス',
+    'Password' => 'パスワード',
+    'Remember Me' => 'ログイン情報を記憶する',
+    'Forgot Your Password?' => 'パスワードを忘れた方はこちら',
+    'Send Password Reset Link' => 'パスワード再セットメール送信',
+    'Reset Password' => 'パスワードリセット',
+    'Name' => '氏名',
+    'Confirm Password' => 'パスワード確認',
+    'Register' => '新規登録',
+];
