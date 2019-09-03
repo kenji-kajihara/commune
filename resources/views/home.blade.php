792b85d8169af580fcab85e76339b4c81d59b4b7
kenji <kenji.kajihara712@gmail.com>
2019-09-03 08:11:08 +0000

    UPDATE : design in home.blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,8 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-body">
+            <div class="login-box card">
+                <div class="card-body mx-auto">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
@@ -13,10 +13,10 @@
                     @endif
 
                    <h2> You are logged in!</h2>
-                   
-                    <div class="col-md-12">
-                        <a href="{{ action('ProfileController@add') }}" role="button" class="btn bt-block btn-primary">プロフィール作成</a>
-                        <a href="{{ action('CommunityController@index') }}">登録済みの方はこちら</p>
+                   <div class="row">
+                        <div class="offset-md-3">
+                            <a href="{{ action('ProfileController@add') }}" role="button" class="btn bt-block btn-primary">プロフィール作成</a>
+                        </div>
                     </div>
                 </div>
             </div>
