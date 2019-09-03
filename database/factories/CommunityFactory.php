06bfd1fc13a875cd429859dcb648dcee369030e6
kenji <kenji.kajihara712@gmail.com>
2019-09-03 08:04:56 +0000

    ADD : CommunitiesTableSeeder

--- database/factories/CommunityFactory.php
+++ database/factories/CommunityFactory.php
@@ -5,8 +5,10 @@
 use App\Model;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(App\Community::class, function (Faker $faker) {
     return [
-        //
+            'user_id' => $faker->numberBetween(1,10),
+            'name' => $faker->word,
+            'description' => $faker->sentence,
     ];
 });
