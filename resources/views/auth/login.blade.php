658a06f4670fc8c1cf3ed23c2cd7e64b959fc4a0
kenji <kenji.kajihara712@gmail.com>
2019-09-13 03:14:29 +0000

    ADD : test user

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,7 +14,7 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('messages.E-Mail Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalide' : '' }} " name="email" value="{{ old('email') }}" required autofocus>
+                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalide' : '' }} " name="email" value="test@test.com" required autofocus>
 
                                 @if  ($errors->has('email'))
                                     <span class="invalid-feedback">
@@ -28,7 +28,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('messages.Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control {{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required >
+                                <input id="password" type="password" class="form-control {{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" value="00000000" required >
 
                                 @if ($errors->has('password'))
                                     <span class="invalid-feedback">
